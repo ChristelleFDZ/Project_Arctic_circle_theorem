dcb1104813b850637eead8791c01ca1d0d5bf4c5
ChristelleFDZ <christelle.fernandez@etu.umontpellier.fr>
2021-04-21 15:05:50 +0200

    theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,8 +18,8 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'Project_Arctic_circle_theorem-'
-copyright = '2021, ESSBAI DJOUAHRA SOW FERNANDEZ'
-author = 'ESSBAI DJOUAHRA SOW FERNANDEZ'
+copyright = '2021, ESSBAI, DJOUAHRA, SOW, FERNANDEZ'
+author = 'ESSBAI, DJOUAHRA, SOW, FERNANDEZ'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1'
@@ -46,7 +46,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
