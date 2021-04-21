dfcf36c9ea83005ff919b969bfcb307da448e3ca
AnnaS <oumouratou-anna.sow@etu.umontpellier.fr>
2021-04-21 18:55:07 +0200

    tests

--- tests/testaztec.py
+++ tests/testaztec.py
@@ -1,5 +1,8 @@
 import unittest
-
+import sys 
+sys.path.append("..") # for getting out of tests directory
+#from src.aztecdiamond import aztecdiamond
+# l'importation de aztecdiamond failed
 class test_aztect_instance(unittest.TestCase):
     def test_aztec_array(self):
         gda = generate_diamond_array(self)
@@ -7,10 +10,17 @@ class test_aztect_instance(unittest.TestCase):
 
     def test_aztec_grille(self):
         gda = production_rect_grille(self)
-        self.assertIsInstance(gda.grille_rects,production_rect_grille)    
+        self.assertIsInstance(gda.grille_rects,production_rect_grille)  
+
+    def test_order_is_positive(self):
+        self.assertGreater(aztecdiamond.order,0)  
+
+    def test_order_is_int(self):
+        self.assertTrue(aztecdiamond.order is int )
 
 if __name__ == '__main__':
     unittest.main()   
       
-#Ran 2 tests in 0.001s
-#FAILED (errors=2)
+#Ran 4 tests in 0.001s
+#FAILED (errors=4)
+#Error : No module named 'src'
--- tests/testdomino.py
+++ tests/testdomino.py
@@ -1,18 +1,25 @@
 import unittest
 import numpy as np
-
+import sys 
+sys.path.append("..") # for getting out of tests directory
+#from src.domino import Domino
+#  l'importation de Domino failed
 class testdomino(unittest.TestCase):
     def test_ifonction_is_instance_of_domino(self): # la fonction __init__
-        d = Domino(angle_droit_h[1],np.array([-1, 0]),order=None )
+        d = Domino(angle_droit_h[1],np.array([-1, 0]),1 )
         self.assertIsInstance(d, Domino)
 
     def test_domino_rectangle(self):
-        d = gen_rect(self, order)
+        d = gen_rect(1)  # order=1
         self.assertIsInstance(d,gen_rect)
+
+    def test_orientation_is_in_ORIENTATIONS(self):
+        self.assertIn(Domino.orientation, ORIENTATIONS)
+
 #Méthode test : "construction de l'objet" 
 
 if __name__ == '__main__':
     unittest.main()     
-# OK
-#Ran 2 tests in 0.001s
-#FAILED (errors=2)
+#Ran 3 tests in 0.001s
+#FAILED (errors=3)
+#Error : No module named 'src'
