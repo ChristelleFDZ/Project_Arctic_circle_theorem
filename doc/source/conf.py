08acf2acdc616d900c1838f1361acda632971b44
ChristelleFDZ <christelle.fernandez@etu.umontpellier.fr>
2021-04-21 15:39:01 +0200

    rst

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,7 +40,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-source_suffix = '.md'
+source_suffix = '.rst'
 
 # -- Options for HTML output -------------------------------------------------
 
